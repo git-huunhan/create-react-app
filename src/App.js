3e15f78c938e977de6e892cfcba318082071cc75
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-12 19:07:29 +0700

    Using props to render item and change style code with constructor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.todoItems = [
+      'Mua bánh',
+      'Mua kẹo',
+      'Mua nước ngọt'
+    ];
+  }
+
   render(){
     return (
       <div className="App">
         <h2>List Item</h2>
-        <TodoItem title="Mua bánh"/>
-        <TodoItem title="Mua kẹo"/>
-        <TodoItem title="Mua nước ngọt"/>
+        {
+          this.todoItems.map((item, index) =>
+          <TodoItem key={index} title={item}/>
+          )
+        }
       </div>
     );
   }
