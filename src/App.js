bb339ab0addb73985f67e844478df42f2f50a992
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-13 20:44:35 +0700

    Using onClick event to click item and using module classnames

--- src/App.js
+++ src/App.js
@@ -5,28 +5,58 @@ import TodoItem from './components/TodoItem';
 class App extends Component {
   constructor(){
     super();
+    this.state = {
+        todoItems: [
+        { title: 'Mua bánh', isComplete: true },
+        { title: 'Mua kẹo', isComplete: true  },
+        { title: 'Mua nước ngọt' }
+      ]
+    };
+  }
 
-    this.todoItems = [
-      // { title: 'Mua bánh', isComplete: true },
-      // { title: 'Mua kẹo', isComplete: true  },
-      // { title: 'Mua nước ngọt' }
-    ];
+  onItemClicked(item){
+    return (event) => {
+      console.log(item);
+      const isComplete = item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    };
   }
 
   render(){
-    return (
-      <div className="App">
-        <h2>List Item</h2>
-        {
-          this.todoItems.length > 0 && this.todoItems.map((item, index) =>
-          <TodoItem key={index} item={item}/>
-          )
-        }
-        {
-          this.todoItems.length === 0 && 'Nothing here'
-        }
-      </div>
-    );
+    const { todoItems } = this.state;
+    if (todoItems.length){
+      return (
+        <div className="App">
+          <p id="header">Todo List</p>
+          {
+            todoItems.length && todoItems.map((item, index) =>
+            <TodoItem key={index} 
+                      item={item} 
+                      onClick={this.onItemClicked(item)}/>
+            )
+          } 
+        </div>
+      );
+    } else {
+      return(
+        <div className="App">
+          <p id="header">Todo List</p>
+          <p id="nothing">Nothing here!</p>
+        </div> 
+      );
+    }
   }
 }
 
